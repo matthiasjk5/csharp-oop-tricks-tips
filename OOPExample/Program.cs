02e8b7e6e25262d2a3377ff5e24735fca39efb60
matthiasjk5 <matthias.jk5@gmail.com>
2019-10-31 13:37:53 -0400

    Add project files.

---
+++ OOPExample/Program.cs
@@ -0,0 +1,34 @@
+﻿using OOPExample;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var comp = new Desktop("Generic Computer", Desktop.CaseType.Tower);
+        //comp.name = "Generic Computer";
+
+        //var comp = new Laptop("Generic Laptop", 1024, 768);
+
+        Console.WriteLine("Computer type - " + comp.GetType());
+
+        var isComputer = comp is Computer;
+
+        Console.WriteLine("Is Computer - " + isComputer);
+        Console.WriteLine("Computer name - " + comp.name);
+        Console.WriteLine("Computer case - " + comp.caseType);
+
+        comp.TogglePower();
+        Console.WriteLine("\nComputer is on - " + comp.isOn);
+        
+        comp.ToggleSleep();
+        Console.WriteLine("\nComputer is sleeping - " + comp.isSleeping);
+        
+        comp.TogglePower();
+        Console.WriteLine("\nComputer is on - " + comp.isOn);
+
+
+        Console.Read();
+    }
+}
+
